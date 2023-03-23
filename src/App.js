096cceb12ac5d1fa9646c993765d146a9441d6e0
Arpan Pandey <95731821+MrShadow2@users.noreply.github.com>
2023-03-23 16:33:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path="/channel/:id" element={<Channel/>} />
         
           <Route path="/search/:searchTerm" element={<Search/>} />
+              <Route path="*" element={<Feed/>} />
         </Routes>
       </Box>
         
